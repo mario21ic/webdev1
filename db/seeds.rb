8a017ff66b42b9ebf415a49ee2c5a1678b7986ec
Arnold <arnoldmezaacosta@gmail.com>
2020-11-30 13:41:56 -0500

    login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Usuario.create(nombre: 'provincias', correo_corporativo:'administrador@grupo1.com',contraseña:'1234')
