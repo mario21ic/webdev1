dd941e3549cd7f591472af07591638297af24f56
heovyh <heovyh@gmail.com>
2020-11-29 19:05:21 -0500

    depa home

--- app/controllers/departamentos_controller.rb
+++ app/controllers/departamentos_controller.rb
@@ -1,8 +1,8 @@
 class DepartamentosController < ApplicationController
   before_action :init, only: [:show, :edit, :update, :destroy, :index, :new,:home]
   before_action :set_departamento, only: [:show, :edit, :update, :destro, :home]
-  before_action :set_proyecto, only: [:new,:index,:new]
-  before_action :set_proyecto2, only: [:show,:edit]
+  before_action :set_proyecto, only: [:new,:index]
+  before_action :set_proyecto2, only: [:show,:edit,:home]
 
   # GET /departamentos
   # GET /departamentos.json
