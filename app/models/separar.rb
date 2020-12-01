b73055b2a71405dff6acd35994da06406535b7d6
Mario Inga <mario21ic@gmail.com>
2020-11-30 20:42:03 -0500

    feat(separar, solicitud, venta) agregando validaciones

--- app/models/separar.rb
+++ app/models/separar.rb
@@ -1,3 +1,33 @@
 class Separar < ApplicationRecord
   belongs_to :departamento, optional: true
+    validates :nombres, presence: {
+        message: "es requerido"
+    }
+    validates :apellidos, presence: {
+        message: "es requerido"
+    }
+
+    validates :dni, presence: {
+        message: "es requerido"
+    }, length: {
+        minimum: 8,
+        too_short: "debe tener 8 digitos"
+    }, uniqueness: {
+        message: "ya esta registrado"
+    }
+
+    validates :voucher, presence: {
+        message: "es requerido"
+    }
+
+    validates :monto, presence: {
+        message: "es requerido"
+    }
+
+    validates :correo, presence: {
+        message: "es requerido"
+    }, uniqueness: {
+        message: "ya esta registrado",
+        case_sensitive: false
+    }
 end
--- app/models/ventum.rb
+++ app/models/ventum.rb
@@ -1,3 +1,30 @@
 class Ventum < ApplicationRecord
   belongs_to :departamento, optional: true
+
+    validates :nombres, presence: {
+        message: "es requerido"
+    }
+    validates :apellidos, presence: {
+        message: "es requerido"
+    }
+
+    validates :numero_documento, presence: {
+        message: "es requerido"
+    }, length: {
+        minimum: 8,
+        too_short: "debe tener 8 digitos"
+    }, uniqueness: {
+        message: "ya esta registrado"
+    }
+
+    validates :telefono, presence: {
+        message: "es requerido"
+    }
+
+    validates :correo, presence: {
+        message: "es requerido"
+    }, uniqueness: {
+        message: "ya esta registrado",
+        case_sensitive: false
+    }
 end
