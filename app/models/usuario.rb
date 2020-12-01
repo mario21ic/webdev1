8949d0c37fb2a1d5936f654b93f858760bf6ed2d
Mario Inga <mario21ic@gmail.com>
2020-11-30 20:32:55 -0500

    feat(usuario) agregando validaciones

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,30 @@
 class Usuario < ApplicationRecord
+    validates :nombre, presence: {
+        message: "es requerido"
+    }
+    validates :apellido, presence: {
+        message: "es requerido"
+    }
+    validates :apellido_materno, presence: {
+        message: "es requerido"
+    }
+
+    validates :numero_documento, presence: {
+        message: "es requerido"
+    }, length: {
+        minimum: 8,
+        too_short: "debe tener 8 digitos"
+    }, uniqueness: {
+        message: "ya esta registrado"
+    }
+
+    validates :correo_corporativo, presence: {
+        message: "es requerido"
+    }, uniqueness: {
+        message: "ya esta registrado",
+        case_sensitive: false
+    }
+    validates :contraseña, presence: {
+        message: "es requerido"
+    }
 end
